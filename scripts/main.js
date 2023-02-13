657d8f04bc6175ef474d92db0df37e87b3457b17
quangtruong06 <truongnobi.hy@gmail.com>
2023-02-13 16:25:05 +0900

    new

---
+++ scripts/main.js
@@ -0,0 +1,37 @@
+$(".nav-button li").click(function () {
+  var text = $(this).text();
+  $(".nav-button li").removeClass("active");
+  $(this).addClass("active");
+  switch (text) {
+    case "1":
+      $(".ads .content .title").html("豊橋駅ビル<br />カルミア");
+      $(".ads .content .description").text(
+        "毎日暮らしのサポート・生活ステーション"
+      );
+      break;
+    case "2":
+      $(".ads .content .title").html("おはようございます。");
+      $(".ads .content .description").text(
+        "毎日暮らしのサポート・生活ステーション"
+      );
+      break;
+    case "3":
+      $(".ads .content .title").html("こんにちは");
+      $(".ads .content .description").text(
+        "毎日暮らしのサポート・生活ステーション"
+      );
+      break;
+    case "4":
+      $(".ads .content .title").html("こんばんは");
+      $(".ads .content .description").text(
+        "毎日暮らしのサポート・生活ステーション"
+      );
+      break;
+    case "5":
+      $(".ads .content .title").html("さようなら");
+      $(".ads .content .description").text(
+        "毎日暮らしのサポート・生活ステーション"
+      );
+      break;
+  }
+});
